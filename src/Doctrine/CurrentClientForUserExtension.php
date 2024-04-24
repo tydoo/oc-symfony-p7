9eb926f4dbf1895b09fbcf66c8f759faced28546
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-04-24 17:33:09 +0200

    ✅ Restriction des données user en fonction du client #6

---
+++ src/Doctrine/CurrentClientForUserExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Doctrine;
+
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use ApiPlatform\Metadata\Operation;
+use Symfony\Bundle\SecurityBundle\Security;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+
+class CurrentClientForUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface {
+
+    public function __construct(private readonly Security $security) {
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        Operation $operation = null,
+        array $context = []
+    ): void {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        Operation $operation = null,
+        array $context = []
+    ): void {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void {
+        if (User::class !== $resourceClass || null === $client = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.client = :client', $rootAlias))
+            ->setParameter('client', $client);
+    }
+}
