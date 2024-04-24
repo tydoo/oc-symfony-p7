07cc1d85d1e4c7925a3777d1c9787c339d50b293
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-04-24 22:00:43 +0200

    🗑️ Clear code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
