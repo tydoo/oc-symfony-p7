faf38a309cbcc5e812f7245292f2fc343f2650f3
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-04-25 12:51:48 +0200

    ♻️ Refactor du Post

--- src/State/UserProcessor.php
+++ src/State/UserProcessor.php
@@ -4,28 +4,24 @@ namespace App\State;
 
 use App\Entity\User;
 use ApiPlatform\Metadata\Operation;
-use Doctrine\ORM\EntityManagerInterface;
 use ApiPlatform\State\ProcessorInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class UserProcessor implements ProcessorInterface {
 
     public function __construct(
         private readonly Security $security,
-        private readonly EntityManagerInterface $entityManager
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
+        private readonly ProcessorInterface $persistProcessor,
     ) {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []) {
         if ($data instanceof User) {
             $data->setClient($this->security->getUser());
-
-            $this->entityManager->persist($data);
-            $this->entityManager->flush();
-            return $data;
         }
 
-        throw new BadRequestException('Name is required');
+        return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
